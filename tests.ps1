5d652d7172371aa27f9552d0d32b43c4168d827c
dennie <dennie.westin@gmail.com>
2015-11-08 01:52:12 +0100

    Test auth

--- tests.ps1
+++ tests.ps1
@@ -1,9 +1,9 @@
 scriptcs "$PSScriptRoot\Automation.Tests.Integration\Run.csx" -debug -loglevel Error 
 scriptcs "$PSScriptRoot\Automation.Tests.Unit\Run.csx" -debug -loglevel Error 
 
-Write-Host "`n------------------------------" -NoNewLine
+Write-Host "`n-------------------------------" -NoNewLine
 Write-Host " Test results " -ForegroundColor White -NoNewLine
-Write-Host "----------------------------------`n" -NoNewLine
+Write-Host "-----------------------------------`n" -NoNewLine
 Write-Host "`nIntegration Tests:`n$PSScriptRoot\Automation.Tests.Integration\BDDfy.html" -ForegroundColor Blue
 Write-Host "`nUnit Tests:`n$PSScriptRoot\Automation.Tests.Unit\BDDfy.html" -ForegroundColor Blue
 Write-Host "`n------------------------------------------------------------------------------`n"
